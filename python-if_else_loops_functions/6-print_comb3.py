11f29bc0760445cf8a752026ce9ab195728ad729
raycast5 <ray.j.colon@gmail.com>
2022-09-08 23:37:46 -0400

    fixed PEP again

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for x in range(9):
     for y in range(x, 10):
         if x != y:
             print("{:d}{:d}".format(x, y),
-                end=", " if x != 8 else "\n")
+                   end=", " if x != 8 else "\n")
