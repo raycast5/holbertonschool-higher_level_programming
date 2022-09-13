b90c536eacfdcccdb6d2cf4ec3049e72dff4271b
raycast5 <ray.j.colon@gmail.com>
2022-09-13 15:04:18 -0400

    Added file 1

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This function retrieves an element of a list in a c like fashion"""
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return None
+    else:
+        return my_list[idx]
