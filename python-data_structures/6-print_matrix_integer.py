77bb710119ca36f4433cb5d1dd53d0ef13af8f6f
raycast5 <ray.j.colon@gmail.com>
2022-09-13 19:10:26 -0400

    Fixed spaces in file 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
         for a in matrix:
             for b in a:
-                print("{:d}".format(b), end=" ")
+                print("{:d}".format(b), end=" " if b != a[-1] else "")
             print()
