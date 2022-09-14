255f1de9b121f795e811bd876e644072b7214324
raycast5 <ray.j.colon@gmail.com>
2022-09-13 21:31:22 -0400

    Added file 8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This function returns a tuple with the length of a string and its first character"""
+
+
+def multiple_returns(sentence):
+    a = len(sentence)
+    b = sentence[0] if a > 0 else None
+    return a, b
