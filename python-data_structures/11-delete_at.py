1b395f2dac44068d744d9e43a24c2e78fd9e52b8
raycast5 <ray.j.colon@gmail.com>
2022-09-13 23:04:14 -0400

    Added file 11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This function deletes an item from list at specified index"""
+
+
+def delete_at(my_list=[], idx=0):
+    if idx in range(1, len(my_list)):
+        del my_list[idx]
+    return my_list
