a9289550a7df75cd0717b6e8854637b661ed448e
raycast5 <ray.j.colon@gmail.com>
2022-09-15 16:05:24 -0400

    Fixed file 12 for None and not string input

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,20 @@
 """This function converts roman numerals to int"""
 
 
+import string
+
+
 def roman_to_int(roman_string):
     total = 0
     roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50,
                   "C": 100, "D": 500, "M": 1000}
-    for i in range(len(roman_string) - 1, -1, -1):
-        integ = roman_dict[roman_string[i]]
-        if integ * 3 < total:
-            total -= integ
-        else:
-            total += integ
-    return total
+    if roman_string is not string or roman_string is None:
+        return 0
+    else:
+        for i in range(len(roman_string) - 1, -1, -1):
+            integ = roman_dict[roman_string[i]]
+            if integ * 3 < total:
+                total -= integ
+            else:
+                total += integ
+        return total
