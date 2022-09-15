e5750276a5c4ce7b1536de27143a6bc11f5835a3
raycast5 <ray.j.colon@gmail.com>
2022-09-15 16:14:28 -0400

    Fixed type check for input

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,9 +9,7 @@ def roman_to_int(roman_string):
     total = 0
     roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50,
                   "C": 100, "D": 500, "M": 1000}
-    if roman_string is not string or roman_string is None:
-        return 0
-    else:
+    if isinstance(roman_string, str):
         for i in range(len(roman_string) - 1, -1, -1):
             integ = roman_dict[roman_string[i]]
             if integ * 3 < total:
@@ -19,3 +17,5 @@ def roman_to_int(roman_string):
             else:
                 total += integ
         return total
+    else:
+        return 0
