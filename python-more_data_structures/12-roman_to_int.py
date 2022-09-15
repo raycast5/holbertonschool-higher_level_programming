b030da648a290a32dcc2dffd2a1c23aa02203692
raycast5 <ray.j.colon@gmail.com>
2022-09-15 15:57:23 -0400

    Added file 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This function converts roman numerals to int"""
+
+
+def roman_to_int(roman_string):
+    total = 0
+    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50,
+                  "C": 100, "D": 500, "M": 1000}
+    for i in range(len(roman_string) - 1, -1, -1):
+        integ = roman_dict[roman_string[i]]
+        if integ * 3 < total:
+            total -= integ
+        else:
+            total += integ
+    return total
