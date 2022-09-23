f32cf860f98b3d40c4c121f349704c4fde8b590e
raycast5 <ray.j.colon@gmail.com>
2022-09-23 15:14:00 -0400

    Added file 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""""Create a class"""
+
+
+class Square:
+    """"A new class called square
+
+    Args:
+        size (int): Integer representing size of square
+
+    """
+
+    def __init__(self, size):
+        self.size = size
