ad3e54eaadfd5514aa806c3904eddc5ab9e07859
raycast5 <ray.j.colon@gmail.com>
2022-09-23 17:14:47 -0400

    Added file 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""""Create a class"""
+
+
+from ctypes import sizeof
+from msilib.schema import Property
+
+
+class Square:
+    """"A new class called square
+
+    Args:
+        size (int): Integer representing size of square
+
+    """
+
+    def __init__(self, size=0):
+        
+            self.__size = size
+            
+    @property
+    def size(self):
+        return self.__height
+    @size.getter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    def area(self):
+        return int(self.__size) * int(self.__size)
