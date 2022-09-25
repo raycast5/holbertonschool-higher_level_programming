2985cc6be6fd249d5f204d7b95b26cb565eab61e
raycast5 <ray.j.colon@gmail.com>
2022-09-25 00:59:24 -0400

    Added file 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""""Create a class"""
+
+
+class Square:
+    """"A new class called square
+
+    Args:
+        size (int): Integer representing size of square
+
+    """
+
+    def __init__(self, size=0):
+
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return int(self.__size) * int(self.__size)
+
+    def my_print(self):
+        size = self.size
+        if size == 0:
+            print("")
+        else:
+            for row in range(size):
+                for item in range(size):
+                    print("#", end="")
+                print("")
