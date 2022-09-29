6a9ef4e93403b5e060727c9c7942625c2b5ce84b
raycast5 <ray.j.colon@gmail.com>
2022-09-29 03:08:40 -0400

    corrected test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,7 +10,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([10, 2, 5, 15.5, 7, 4, 2]), 15.5)
         self.assertEqual(max_integer([10, 2, 5, -15, 7, -20, 2]), 10)
         self.assertEqual(max_integer([10, 2, 5, 15, 7, 2, 20]), 20)
-        self.assertEqual(max_integer([2]), 20)
+        self.assertEqual(max_integer([2]), 2)
 
         
     def test_empty(self):
