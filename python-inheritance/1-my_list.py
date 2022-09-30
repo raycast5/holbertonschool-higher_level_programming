63e0e3e3b0bedc75c12662a3b382c1cc0d9d91b7
raycast5 <ray.j.colon@gmail.com>
2022-09-30 16:12:35 -0400

    Added file 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains a class MyList that inherits from list"""
+
+
+class MyList(list):
+    """A class that inherits from list"""
+
+    def print_sorted(self):
+        """Prints a sorted list of ints"""
+
+        print(self.sort)
