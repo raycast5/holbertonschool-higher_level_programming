5d9d91c8f9032e1327c40328a9c633636f714167
raycast5 <ray.j.colon@gmail.com>
2022-09-30 17:38:35 -0400

    Added file 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module contains a function for testing"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Checks if object is an instance of a class that
+    inherited (directly or indirectly) from
+    the specified class
+
+    Args:
+    obj: an object
+    a_class: class to check against
+
+    Returns:
+    True if object is an instance of a class that
+    inherited (directly or indirectly) from
+    the specified class; otherwise False.
+
+    """
+
+    return issubclass(obj, a_class)
