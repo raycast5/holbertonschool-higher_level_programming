6fa93ae4b71cd13a70d47a89684854c277db68a5
raycast5 <ray.j.colon@gmail.com>
2022-09-30 18:38:34 -0400

    Added file 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module contains a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    An empty class
+
+    Attributes: None
+    """
+
+    def __init__(self):
+        """Init method"""
+
+        pass
+
+    def area(self):
+        """Empty function"""
+
+        raise Exception("area() is not implemented")
