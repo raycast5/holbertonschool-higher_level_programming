13cdfbc338ae5f25c8726e1fee7042b8721c4407
raycast5 <ray.j.colon@gmail.com>
2022-09-30 17:28:43 -0400

    Added file 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module contains a lookup function for testing"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if the object is exactly an
+    instance of the specified class.
+
+    Args:
+    obj: an object
+    a_class: class to check against
+
+    Returns:
+    True if the object is exactly an instance of
+    the specified class ; otherwise False.
+
+    """
+
+    return type(obj) is a_class
