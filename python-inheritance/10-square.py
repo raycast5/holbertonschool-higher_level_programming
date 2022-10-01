e8ae444cc1f4cdf04b3f1b1aa872b89552187a37
raycast5 <ray.j.colon@gmail.com>
2022-09-30 21:41:27 -0400

    Added file 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module contains a class BaseGeometry"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A subclass of Rectangle
+
+    Attributes: None
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+
+        self.__height = size
+        self.__width = size
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__width}/{self.__height}")
+
+    def area(self):
+        return self.__height * self.__width
