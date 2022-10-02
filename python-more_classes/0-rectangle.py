29564bb6de0669572118dbbbee328938f22c975b
raycast5 <ray.j.colon@gmail.com>
2022-10-02 16:11:24 -0400

    Added file 0 and readme

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""Create a class"""
+
+
+class Rectangle:
+    """"A new class called Rectangle
+
+    Args:
+        size (int): Integer representing size of square
+
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+    This directory is for practicing Object Oriented Programming, object and classes.
