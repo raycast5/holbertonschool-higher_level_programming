996d77a7d3e0cc4265da281f0be3cabcd5e73b7a
raycast5 <ray.j.colon@gmail.com>
2022-10-02 16:34:05 -0400

    Fixed file 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -35,8 +35,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if isinstance(value, int) is False:
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height = value
