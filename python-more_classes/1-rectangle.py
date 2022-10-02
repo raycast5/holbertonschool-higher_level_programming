d73ef3e80ea6cf6cf2058acf0f312a1f57516e46
raycast5 <ray.j.colon@gmail.com>
2022-10-02 16:29:13 -0400

    Added file 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""""Create a class"""
+
+
+class Rectangle:
+    """"A new class called Rectangle
+
+    Args:
+        width (int): Integer representing width of square
+        height (int):  Integer representing height of square
+
+    """
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__height = value
