47e50bcb7b87329536d689cede02da96bec94ae1
raycast5 <ray.j.colon@gmail.com>
2022-10-04 12:48:19 -0400

    Added file  3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains a function for testing"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)
+
+    Args:
+    my_obj: string to be serialized
+
+    Returns:
+    JSON representation of an object (string)
+
+    """
+
+    return json.dump(my_obj)
