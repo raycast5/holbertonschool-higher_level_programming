19fa3a45ddc1fe8c08332fc53664bf48b5f10ae4
raycast5 <ray.j.colon@gmail.com>
2022-10-04 14:38:07 -0400

    Added file 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module contains a function for testing"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an Object from a “JSON file”:
+
+    Args:
+    filename: name of file to read from
+
+    Returns:
+    Nothing
+
+    """
+
+    with open(filename, "r+", encoding="utf-8") as file1:
+        return json.load(file1)
