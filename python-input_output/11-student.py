a84f613e79432c2de49b6b0ad70845ac41acbab8
raycast5 <ray.j.colon@gmail.com>
2022-10-04 18:41:37 -0400

    Added file 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""""Module that contains a class named Student"""
+
+
+class Student:
+    """"
+    A new class called Student
+
+    Args:
+    first_name (str): First name of student
+    last_name (str): Last name of student
+    age (int): Age of student
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        json_dic = self.__dict__
+        if type(attrs) is list:
+            if all(isinstance(s, str) for s in attrs):
+                return {k: v for k, v in json_dic.items() if k in attrs}
+        else:
+            return json_dic
+    
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            self.k = v
