ecb6c2293a2dfd4399751ee47b815f81d0525411
raycast5 <ray.j.colon@gmail.com>
2022-10-04 12:58:01 -0400

    Added file 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains a function for testing"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """returns an object represented by a JSON string:
+
+    Args:
+    my_str: string to be deserialized
+
+    Returns:
+    an object represented by a JSON strin
+
+    """
+
+    return json.load(my_str)
