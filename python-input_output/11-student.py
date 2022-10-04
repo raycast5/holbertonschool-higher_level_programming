cfcd113dc4be694e2f3f412f7b401dff3b973533
raycast5 <ray.j.colon@gmail.com>
2022-10-04 19:11:29 -0400

    Edited file 11

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -28,5 +28,4 @@ class Student:
     
     def reload_from_json(self, json):
         for k, v in json.items():
-            if k in self.__dict__:
-                self.k = v
+            self.__dict__[k] = v
