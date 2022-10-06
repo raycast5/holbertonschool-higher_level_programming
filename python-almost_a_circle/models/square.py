d5c2c2998f09e27f7ebc31c757d31ef853445f11
raycast5 <ray.j.colon@gmail.com>
2022-10-06 17:15:37 -0400

    Fixed square getter and setter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,19 +17,13 @@ class Square(Rectangle):
     @property
     def size(self):
         """Retrieves Square size"""
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         """Sets Square size."""
-
-        if isinstance(value, int) is False:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.width = value
-            self.height = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Returns a str representation of square"""
