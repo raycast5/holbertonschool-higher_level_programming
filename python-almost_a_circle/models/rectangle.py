a810a76deab775dee36bb7e5d70fd8a40237ff8b
raycast5 <ray.j.colon@gmail.com>
2022-10-06 14:40:11 -0400

    Added update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -118,3 +118,12 @@ class Rectangle(Base):
 
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - " +
                 f"{self.__width}/{self.__height}")
+
+    def update(self, *args):
+        """Updates parameters of Rectangle"""
+
+        self.id = args[0]
+        self.__width = args[1]
+        self.__height = args[2]
+        self.__x = args[3]
+        self.__y = args[4]
