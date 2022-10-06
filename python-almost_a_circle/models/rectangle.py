68e274f574b422a77583900d6762faca1e968ee5
raycast5 <ray.j.colon@gmail.com>
2022-10-06 13:03:56 -0400

    Added class rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+"""This module contains a class BaseGeometry"""
+
+
+from models import Base
+
+
+class Rectangle(Base):
+    """A subclass of Base that makes a new rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Constructs a new rectangle
+
+        Args:
+            width (int): Integer representing width of rectangle
+            height (int):  Integer representing height of rectangle
+            x (int): Coords of rectangle
+            y (int): Coords of rectangle
+
+        Raises:
+            TypeError: If input is not an int
+            ValueError: If width or height <= 0 and x or y < 0
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Retrieves rectangle width."""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets rectangle width."""
+
+        if isinstance(value, int) is False:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Retrieves rectangle height."""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets rectangle height."""
+
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
+
+    @property
+    def x(self):
+        """Retrieves x coord of rectangle."""
+
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets x coord of rectangle."""
+
+        if isinstance(value, int) is False:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        """Retrieves y coord of rectangle."""
+
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets y coord of rectangle."""
+
+        if isinstance(value, int) is False:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
