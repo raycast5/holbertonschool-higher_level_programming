f3a266b21551d758292e7d38d3feb5ded5b59dec
raycast5 <ray.j.colon@gmail.com>
2022-10-06 22:46:36 -0400

    Added staticmethod to base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """"Module containing a class"""
 
+import json
+
 
 class Base:
     """"A new class called Base
@@ -17,3 +19,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
