63655fcd8f1b9f765bda9592a48d58f8484b7399
raycast5 <ray.j.colon@gmail.com>
2022-10-08 00:02:39 -0400

    Added normal tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -12,12 +12,14 @@ class TestRectangle(unittest.TestCase):
         cls.r1 = Rectangle(1, 2, 3, 4)
         cls.r2 = Rectangle(1, 2)
         cls.r3 = Rectangle(1, 2, 3)
-    """
+    
     def test_normal(self):
         self.assertEqual(self.r1.width, 1)
         self.assertEqual(self.r1.height, 2)
         self.assertEqual(self.r1.x, 3)
         self.assertEqual(self.r1.y, 4)
-        self.assertEqual(self.r23.width, 1)
-        self.assertEqual(self.r1.height, 2)
-    """
+        self.assertEqual(self.r2.width, 1)
+        self.assertEqual(self.r2.height, 2)
+        self.assertEqual(self.r3.width, 1)
+        self.assertEqual(self.r3.height, 2)
+        self.assertEqual(self.r3.x, 3)
