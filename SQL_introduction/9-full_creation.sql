643af0776d33245d28a6a64166d3260bc33fd362
raycast5 <ray.j.colon@gmail.com>
2022-11-06 09:28:52 -0400

    Added file 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- creates atable with rows at the same time
+CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES ((1,2,3,4), ("John", "Alex", "Bob", "George"), (10, 3, 14, 8));
