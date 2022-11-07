50d85a72e475fc1321edf67905d01deb61ed0506
raycast5 <ray.j.colon@gmail.com>
2022-11-06 22:55:12 -0400

    fix file 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,5 +3,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
-    FOREIGN KEY (sate_id) REFERENCES states(id),
+    FOREIGN KEY (sate_id) REFERENCES hbtn_0d_usa.states(id),
     name VARCHAR(256) NOT NULL);
