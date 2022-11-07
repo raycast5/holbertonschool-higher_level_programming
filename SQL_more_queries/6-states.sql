a1e33070ee778647e499109da30dd86577f5f53d
raycast5 <ray.j.colon@gmail.com>
2022-11-06 22:31:35 -0400

    Added file 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.unique_id (
+    id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL);
