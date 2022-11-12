766f449350ab7f176ef0463da1797a020edda70e
raycast5 <ray.j.colon@gmail.com>
2022-11-11 22:52:48 -0400

    Corrected for capital letter file 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT id, name FROM states WHERE \
-                name like 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
-        print(row)
+        if row[1][0] == "N":
+            print(row)
     cur.close()
     conn.close()
