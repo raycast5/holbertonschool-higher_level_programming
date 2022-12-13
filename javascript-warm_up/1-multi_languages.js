e7796097a72759f0a01bf3a94f802f4e255a1af1
raycast5 <ray.j.colon@gmail.com>
2022-12-12 20:43:53 -0400

    Added semi-colons file 1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // script that prints a few lines
 
-console.log('C is fun')
-console.log('Python is cool')
-console.log('JavaScript is amazing')
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
