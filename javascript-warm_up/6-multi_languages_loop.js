94dab5e2736e78d556940557f513724f4f9f8e51
raycast5 <ray.j.colon@gmail.com>
2022-12-12 21:57:47 -0400

    Added file 6

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// script that prints lines using loops
+
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < langs.length; i++) {
+  console.log(langs[i]);
+}
