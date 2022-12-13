7d5da989220fd388d89c8504cee50a2e053c245e
raycast5 <ray.j.colon@gmail.com>
2022-12-12 21:11:15 -0400

    Added id clause file 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-// script that checks ifther is arguments
+// script that checks if there is arguments
 
 if (process.argv.length <= 2) {
   console.log('No argument');
-} else {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
