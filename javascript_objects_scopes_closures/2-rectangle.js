a536d38098510259fe261349829893296a0aeec4
raycast5 <ray.j.colon@gmail.com>
2022-12-13 22:22:49 -0400

    Added file 2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Creates a class Rectangle that takes w and h
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
