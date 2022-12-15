f933cf48db8290c845fc1c3ac493a1cd572f6d3f
raycast5 <ray.j.colon@gmail.com>
2022-12-15 19:17:44 -0400

    Added file 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Writes a given string to given file
+
+const fs = require('fs');
+const fname = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fname, content, function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
