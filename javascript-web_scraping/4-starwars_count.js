dcbba046894e77815d958de0dc157940bfaa16c5
raycast5 <ray.j.colon@gmail.com>
2022-12-15 23:15:14 -0400

    Added file 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Retreives count of SW appearances for given char id
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    for (const film in films) {
+      const chars = films[film].characters;
+      for (const char in chars) {
+        if (chars[char].includes('18')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
