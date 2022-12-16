1da8d033e357add21db3c97db71e91f92d80e28a
raycast5 <ray.j.colon@gmail.com>
2022-12-15 20:31:26 -0400

    Added file 2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Writes a given string to given file
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, function (error, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
